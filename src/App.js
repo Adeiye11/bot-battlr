7f8702f6c9e79acfeca243d919eae250db5011f1
EstherAdegbaju <princessadeiye@gmail.com>
2023-09-18 20:06:09 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,23 +1,45 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { useState, useEffect } from "react";
+import BotCollection from './Components/BotCollection';
+import YourBotArmy from './Components/YourBotArmy';
 
 function App() {
+  const [botlist, setBotlist] = useState([])
+  const [botArmy, setBotArmy] = useState([])
+
+  useEffect(() => {
+    fetch('http://localhost:3000/bots')
+      .then(result => result.json())
+      .then(data => setBotlist(data))
+  }
+    , []);
+
+  function addBot(id) {
+    const newBot = botlist.find(bot => bot.id === id)
+    setBotArmy([...botArmy, newBot])
+  }
+
+  function removeBot(id) {
+    const newBotArmy = botArmy.filter(bot => bot.id !== id)
+    setBotArmy(newBotArmy)
+  }
+
+  async function deleteBot(id) {
+    const newBotlist = botlist.filter(bot => bot.id !== id)
+    const newBotArmy = botArmy.filter(bot => bot.id !== id)
+    setBotlist(newBotlist)
+    setBotArmy(newBotArmy)
+    const response = await fetch(`http://localhost:3000/bots/${id}`, {
+      method: 'DELETE'
+    })
+    const data = await response.json()
+    console.log(data)
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <YourBotArmy botlist={botlist} botArmy={botArmy} removeBot={removeBot} deleteBot={deleteBot} />
+      <BotCollection botlist={botlist} botArmy={botArmy} addBot={addBot} deleteBot={deleteBot} />
     </div>
   );
 }
