32d578665467e8cd6afb3d93686f772d5f687a6f
EstherAdegbaju <princessadeiye@gmail.com>
2023-09-18 21:00:16 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
   const [botArmy, setBotArmy] = useState([])
 
   useEffect(() => {
-    fetch('https://api.jsonbin.io/v3/b/65089bf95fd7b0304670e543/bots')
+    fetch('https://api.jsonserve.com/xS8ekS')
       .then(result => result.json())
       .then(data => setBotlist(data))
   }
@@ -29,7 +29,7 @@ function App() {
     const newBotArmy = botArmy.filter(bot => bot.id !== id)
     setBotlist(newBotlist)
     setBotArmy(newBotArmy)
-    const response = await fetch(`https://api.jsonbin.io/v3/b/65089bf95fd7b0304670e543/bots/${id}`, {
+    const response = await fetch(`https://api.jsonserve.com/xS8ekS/${id}`, {
       method: 'DELETE'
     })
     const data = await response.json()
